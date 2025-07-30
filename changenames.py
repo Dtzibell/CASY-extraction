74dbf06f0fa35b349171fdcf3de8477033dab2e5
Dtzibell <zaleniakas.tauras@gmail.com>
2025-07-30 15:27:35 +0200

    fix plotting

--- changenames.py
+++ changenames.py
@@ -1,8 +1,26 @@
 from pathlib import Path
 
-for root, dirs, files in Path("Datasets/KWK GC replicate 3").walk():
+medium_dict = {
+        "Et": "ETH",
+        # "Ga": "GAL",
+        # "Gl": "GLU",
+        "Gm": "GMM",
+        "Sm": "SMM"
+        }
+strain_number_dict = {
+        "1":"25",
+        "2":"26",
+        "3":"35"
+        }
+
+for root, dirs, files in Path("Datasets/AP GC replicate 1").walk():
     for file_name in sorted(files):
         file = Path(root) / Path(file_name)
         file_stem = file.stem
-        new_name = Path(root) / Path(file_stem[:-1]+str(int(file_stem[-1])-1)+".raw")
-        file.rename(new_name)
+        medium = file_stem[:1]
+        try:
+            # new_name = Path(root) / Path(medium_dict[medium]+str(int(file_stem[1:])-1)+".raw")
+            new_name = Path(root) / Path(file_stem[:3]+strain_number_dict[file_stem[3]]+file_stem[4:]+".raw")
+            file.rename(new_name)
+        except KeyError:
+            pass
--- extraction.py
+++ extraction.py
@@ -1,31 +1,33 @@
 from pathlib import Path
-from decimal import Decimal
 import polars as pl
 from collections import defaultdict
 import re
-import xlsxwriter as xlw
 import sys
 import math
 import numpy
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-try:
-    number_of_measurements = int(sys.argv[1])
-except ValueError:
-    print("Invalid argument, needs to be int-like.")
-    number_of_measurements = int(input("Enter the number of measurements: "))
+mpl.rc("boxplot.flierprops", marker = ".", markersize = 5, markerfacecolor = "k")
+mpl.rc("boxplot.medianprops", color = "b")
 
-dir_ = Path("Datasets")
+measurement_no = int(sys.argv[1])
+mediums = {
+        "GMM": "1% GMM",
+        "SMM": "1% SMM",
+        "GAL": "SC-Galactose",
+        "GLU": "SC-Glucose",
+        "ETH": "SC-Ethanol"
+        }
+dir_of_raws = Path("Datasets")
 re_diam = re.compile("mean diameter.*range 2.*", re.IGNORECASE)
-re_cellno = re.compile("cells/ml.*range 2.*", re.IGNORECASE)
 
 # Primary sorting is likely the medium, secondary is the strain
-primary_sorting = int(input("Enter the number of symbols determining the primary sorting criterion: "))
-secondary_sorting = int(input("Enter the number of symbols determining the secondary sorting criterion:"))
-data_dict = defaultdict(lambda: defaultdict(list))
-last_measurement_index = -1
+strain_index = int(input("Enter the number of symbols determining the strain: "))
+extracted_data = defaultdict(lambda: defaultdict(list))
+last_measurement_index = -1 # necessary for comparison with index of first measurement
 
-for root, dirs, files in dir_.walk():
+for root, dirs, files in dir_of_raws.walk():
     for file in sorted(files):
 
         file_path = Path(root) / Path(file)
@@ -33,63 +35,65 @@ for root, dirs, files in dir_.walk():
         current_measurement_index = int(file_name[-1])
         
         # case - a measurement was missed in the middle of the experiment
+        # TODO - THIS WILL CRASH IF THE FIRST MEASUREMENT IS NOT INDEXED 0
         if current_measurement_index > last_measurement_index:
             while last_measurement_index != current_measurement_index - 1:
-                for key in data_dict[primary_sorting_criterion].keys():
-                    data_dict[primary_sorting_criterion][key].append(None)
+                for key in extracted_data[primary_sorting_criterion].keys():
+                    extracted_data[primary_sorting_criterion][key].append(None)
                 last_measurement_index += 1
 
-        #case - a measurement was missed at the beginning of the experiment
+        # case - a measurement was missed at the beginning of the experiment
         elif current_measurement_index < last_measurement_index:
-            while last_measurement_index != number_of_measurements - 1:
-                for key in data_dict[primary_sorting_criterion].keys():
-                    data_dict[primary_sorting_criterion][key].append(None)
+            while last_measurement_index != measurement_no - 1:
+                for key in extracted_data[primary_sorting_criterion].keys():
+                    extracted_data[primary_sorting_criterion][key].append(None)
                 last_measurement_index += 1
 
-        primary_sorting_criterion = file_name[:primary_sorting] # If sorting condition is medium, this is the medium.
+        primary_sorting_criterion = file_name[:3]
 
         # case - the first measurement of the first experiment was missed. (needs the new sorting criterion to work)
         if current_measurement_index < last_measurement_index and current_measurement_index > 0:
             last_measurement_index = 0
             while last_measurement_index != current_measurement_index:
-                for key in data_dict[primary_sorting_criterion].keys():
-                    data_dict[primary_sorting_criterion][key].append(None)
+                for key in extracted_data[primary_sorting_criterion].keys():
+                    extracted_data[primary_sorting_criterion][key].append(None)
                 last_measurement_index += 1 
         
         last_measurement_index = current_measurement_index
         with open(file_path) as file:
             lines = file.read()
-            data_dict[primary_sorting_criterion]["file_name"].append(file_name)
-            mean_diameter = re_diam.search(lines).group()
-            left_index = mean_diameter.index("\t") + 1
-            right_index = mean_diameter.rindex(" ")
-            data_dict[primary_sorting_criterion]["mean_diameter [\u03BCl)]"].append(mean_diameter[left_index:right_index])
-            cells_no = re_cellno.search(lines).group()
-            left_index = cells_no.index("\t") + 1
-            cells_no = int(Decimal(cells_no[left_index:])) # Decimal converts the string in scientific notation to a proper int
-            data_dict[primary_sorting_criterion]["cells/ml"].append(cells_no)
-print(data_dict)
-mydict = defaultdict(list)
-for key in data_dict.keys():
-    df = pl.DataFrame(data_dict[key])
-    df = df.with_columns((pl.col("cells/ml").log()).alias("logarithmic_expression"))
+            extracted_data[primary_sorting_criterion]["file_name"].append(file_name)
+            mean_diameter_line = re_diam.search(lines).group()
+            # finds where the mean diameter begins and ends
+            left_of_mean_diam = mean_diameter_line.index("\t") + 1
+            right_of_mean_diam = mean_diameter_line.rindex(" ")
+            extracted_data[primary_sorting_criterion]["mean_diameter [\u03BCl]"].append(float(mean_diameter_line[left_of_mean_diam:right_of_mean_diam]))
+
+# export csv here, plot OD log scale, cell size box plot together
+plotting_data = defaultdict(list)
+for key in extracted_data.keys():
+    medium_data = pl.DataFrame(extracted_data[key])
+    medium_data.write_csv(f"{key}.csv")
     starting_index = 0
-    for i in range(df.height):
-        if (i + 1) % number_of_measurements == 0 and i != 0:
-            growth_rate_df = df[starting_index : i+1].select(pl.col("file_name"), pl.col("logarithmic_expression")).with_row_index("time [h]").drop_nulls().drop_nans()
-            strain = growth_rate_df["file_name"][-1][0:3]
-            exp_growth_rate = numpy.polyfit(growth_rate_df["time [h]"], growth_rate_df["logarithmic_expression"], 1)
-            mydict[strain].append(exp_growth_rate[0])
+    for i in range(medium_data.height):
+        if (i + 1) % measurement_no == 0 and i != 0:
+            cell_sizes = (medium_data[starting_index : i+1].select(pl.col("file_name"), pl.col("mean_diameter [\u03BCl]"))
+                              .with_row_index("time [h]").drop_nulls().drop_nans())
+            strain = cell_sizes["file_name"][-1][0:3+strain_index]
+            average_cell_size = cell_sizes["mean_diameter [\u03BCl]"].sum() / cell_sizes["mean_diameter [\u03BCl]"].len()
+            plotting_data[strain].append(average_cell_size)
             starting_index = i + 1
 
-time_points = list(range(9))
-growthrate_dict = defaultdict(float)
-for idx, key in enumerate(mydict.keys()):
-    growth_rates = math.log(2) / numpy.array(mydict[key])
-    average_growth_rate = sum(growth_rates) / len(growth_rates)
-    strain_curve = [1000000 * 2 ** (i / average_growth_rate) for i in range(9)]
-    plt.plot(time_points, strain_curve, label = f"KWK{key[primary_sorting:primary_sorting+secondary_sorting+1]}")
+boxplot = list()
+labels = list()
+for idx, key in enumerate(plotting_data.keys()):
+    boxplot.append(plotting_data[key])
+    labels.append(f"AP{key[3:3+strain_index]}")
     if (idx + 1) % 3 == 0:
-        plt.legend()
-        plt.savefig(f"{key[:2]}.png")
+        plt.boxplot(boxplot, tick_labels = labels, showmeans = True)
+        plt.xlabel("Strain")
+        plt.ylabel("mean_diameter [\u03BCl]")
+        plt.savefig(f"{mediums[key[:3]]}.png")
         plt.close()
+        boxplot.clear()
+        labels.clear()
